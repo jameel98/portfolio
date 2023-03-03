9314e9d27438dffccfda6a3803258852c2bcce9e
jameel98 <jameelmograbi@gmail.com>
2023-03-03 20:09:38 +0200

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,28 @@
+import React from "react";
+import "./App.css";
+import Header from "./components/header/Header";
+import Nav from "./components/nav/Nav";
+import About from "./components/about/About";
+import Experience from "./components/experience/Experience";
+import Portfolio from "./components/portfolio/Portfolio";
+import Contact from "./components/contact/Contact";
+import Footer from "./components/footer/Footer";
+import Services from "./components/services/Services";
+import Testimonials from "./components/testimonials/Testimonials";
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <Nav />
+      <About />
+      <Experience />
+      <Services />
+      <Portfolio />
+      <Testimonials />
+      <Contact />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
