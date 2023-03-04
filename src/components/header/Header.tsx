52de1ed7fabd8c7a41d20323430862959dfdee55
jameel98 <jameelmograbi@gmail.com>
2023-03-04 23:41:05 +0200

    fixing bugs

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -7,21 +7,23 @@ import HeaderSocials from "./HeaderSocials";
 
 const Header = () => {
   return (
-    <div className="container header__container">
-      <h5> Hello I'm </h5>
-      <h1> Pop</h1>
-      <h5 className=".text-light"> Fullstack Developer</h5>
-      <CTA />
-      <HeaderSocials />
+    <header>
+      <div id="home" className="container header__container">
+        <h5> Hello I'm </h5>
+        <h1> Jameel</h1>
+        <h5 className="text-light"> Fullstack Developer</h5>
+        <CTA />
+        <HeaderSocials />
 
-      <div className="me">
-        <img src={ME} alt="me" />
+        <div className="me">
+          <img src={ME} alt="me" />
+        </div>
+
+        <a href="#contact" className="scroll__down">
+          Scroll Down
+        </a>
       </div>
-
-      <a href="#contact" className="scroll__down">
-        Scroll Down
-      </a>
-    </div>
+    </header>
   );
 };
 
