c2c31e72574e09b02b60fc870453abf2911d8f08
jameel98 <jameelmograbi@gmail.com>
2023-03-30 12:20:48 +0300

    comment unused stuff

--- src/App.tsx
+++ src/App.tsx
@@ -7,8 +7,8 @@ import Experience from "./components/experience/Experience";
 import Portfolio from "./components/portfolio/Portfolio";
 import Contact from "./components/contact/Contact";
 import Footer from "./components/footer/Footer";
-import Services from "./components/services/Services";
-import Testimonials from "./components/testimonials/Testimonials";
+// import Services from "./components/services/Services";
+// import Testimonials from "./components/testimonials/Testimonials";
 
 function App() {
   return (
